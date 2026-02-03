9da1c40af170ccc9b1767f23d16d33eab13ad829
jvargas-rgb <jvargas40456@ufide.ac.cr>
2026-02-03 00:43:04 -0600

    ProyectoProgramacionAvanzadaG8
    Se le agregaron las imágenes y se corrigió el error del login invisible

---
+++ ProgramacionAvanzadaG8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProgramacionAvanzadaG8.MvcApplication" Language="C#" %>
---
+++ ProgramacionAvanzadaG8/Template/js/register.js
@@ -0,0 +1,4 @@
+document.getElementById("registroForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Registro exitoso");
+});
